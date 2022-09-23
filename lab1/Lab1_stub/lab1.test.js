2cbbc6a3e9c4feafef635ba36392abd8fc2fdb78
ULINCHEN <chenyoulinusa@gmail.com>
2022-09-23 19:14:51 -0400

    lab1

--- lab1/Lab1_stub/lab1.test.js
+++ lab1/Lab1_stub/lab1.test.js
@@ -1,25 +1,56 @@
 const lab1 = require('./lab1');
 //TODO: Write and call each function in lab1.js 5 times each, passing in different input
+function deepEqual(a, b) {
+    if (isNaN(a) && isNaN(b)) return true;
+    if ((typeof a == 'object' && a != null) &&
+        (typeof b == 'object' && b != null)) {
+        var count = [0, 0];
+        for (var key in a) count[0]++;
+        for (var key in b) count[1]++;
+        if (count[0] - count[1] != 0) { return false; }
+        for (var key in a) {
+            if (!(key in b) || !deepEqual(a[key], b[key])) { return false; }
+        }
+        for (var key in b) {
+            if (!(key in a) || !deepEqual(b[key], a[key])) { return false; }
+        }
+        return true;
+    }
+    else {
+        return a === b;
+    }
+}
+
+const test = (desc, input, res) => {
+    return (fn) => {
+        if (deepEqual(fn(...input), res)) return desc + "Result: PASS";
+        else return desc + "Result: Fail";
+    }
+}
 
 console.log("function 1 test:");
-console.log(lab1.questionOne([5, 3, 10]));  // Returns and then outputs [true, true, false]
-console.log(lab1.questionOne([2, 1, 2])); // Returns and then outputs[true, false, true] 
-console.log(lab1.questionOne([512, 1007, 17389])); //Returns and then outputs [false, false, true]
-console.log(lab1.questionOne([0, 14159, 785])); //Returns and then outputs [false, true, false]
-console.log(lab1.questionOne([11, 4])); //Returns and then outputs [true, false]
+console.log(test("Case 1 ", [[5, 3, 10]], [true, true, false],)(lab1.questionOne));
+console.log(test("Case 2 ", [[2, 1, 2]], [true, false, true],)(lab1.questionOne));
+console.log(test("Case 3 ", [[512, 1007, 17389]], [false, false, true],)(lab1.questionOne));
+console.log(test("Case 4 ", [[0, 14159, 785]], [false, true, false],)(lab1.questionOne));
+console.log(test("Case 5 ", [[11, 4]], [true, false],)(lab1.questionOne));
 
 console.log("function 2 test:");
-console.log(lab1.questionTwo(5, 3, 10));  // Returns and then outputs 147620 
-console.log(lab1.questionTwo(2, 0, 2)); // Returns and then outputs 0 
-console.log(lab1.questionTwo(512, 1007, -5)); //Returns and then outputs NaN
-console.log(lab1.questionTwo(2, 10, 4)); //Returns and then outputs 2222
-console.log(lab1.questionTwo(175, 3, 5)); //Returns and then outputs 21175 
+console.log(test("Case 1 ", [5, 3, 10], 147620,)(lab1.questionTwo));
+console.log(test("Case 2 ", [2, 0, 2], 0,)(lab1.questionTwo));
+console.log(test("Case 3 ", [512, 1007, -5], NaN,)(lab1.questionTwo));
+console.log(test("Case 4 ", [2, 10, 4], 2222,)(lab1.questionTwo));
+console.log(test("Case 5 ", [175, 3, 5], 21175,)(lab1.questionTwo));
 
 console.log("function 3 test:");
-console.log(lab1.questionThree("How now brown cow"));  // Returns and then outputs 10
-console.log(lab1.questionThree("Welcome to CS-546")); // Returns and then outputs 7 
-console.log(lab1.questionThree("JavaScript is fun!")); //Returns and then outputs 10
+console.log(test("Case 1 ", ["How now brown cow"], 10,)(lab1.questionThree));
+console.log(test("Case 2 ", ["Welcome to CS-546"], 7,)(lab1.questionThree));
+console.log(test("Case 3 ", ["JavaScript is fun!"], 10,)(lab1.questionThree));
+console.log(test("Case 4 ", ["My name is YoulinChen"], 9,)(lab1.questionThree));
 
 console.log("function 4 test:");
-console.log(lab1.questionFour("hello world", "o"));  // Returns and then outputs 2
-console.log(lab1.questionFour("Helllllllo, class!", "ll")); // Returns and then outputs 3 
+console.log(test("Case 1 ", ["hello world", "o"], 2,)(lab1.questionFour));
+console.log(test("Case 2 ", ["Helllllllo, class!", "ll"], 3,)(lab1.questionFour));
+console.log(test("Case 3 ", ["ababababababababababababababababababababab", "ab"], 21,)(lab1.questionFour));
+console.log(test("Case 4 ", ["aaaaaaaaaaaaaaaaaaaa", "a"], 20,)(lab1.questionFour));
+console.log(test("Case 5 ", ["a", " "], 0,)(lab1.questionFour));
